c0ec68983b9317a616ddafbb9d2837e8f0c5cdd7
ramiksaier <ramos.ksaier@gmail.com>
2021-01-31 10:25:45 +0100

    add all the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Navbar, Nav, Form, Button, FormControl,ProgressBar } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">About us </Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Contact us </Nav.Link>
+          <Nav.Link href="#pricing">Sign in</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-info">Search</Button>
+        </Form>
+      </Navbar>
+      <Form className="form">
+      <Form.Control type="text" className="impt"  placeholder="First name ..." readOnly />
+      <Form.Control type="text" className="impt"  placeholder="Last name..." readOnly />
+
+      <Form.Control type="text" className="impt"  placeholder="your email..." readOnly />
+      <Form.Control type="text" className="impt" placeholder="your password..." readOnly />
+      <Form.Control type="text" className="impt"  placeholder="Retape your password..." readOnly />
+
+        <Button variant="primary" className="btnsubmit" type="submit">
+          Submit
+        </Button>
+        <ProgressBar animated now={45} />
+      </Form>
+          <footer class="row">
+          <p class="col-12 text-white text-center tm-copyright-text">
+            Copyright &copy; 2020 App 
+            Designed by Rami ksaier 
+          </p>
+        </footer>
     </div>
   );
 }
